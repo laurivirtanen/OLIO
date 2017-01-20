8f60195fc122adad5419fe4c1086cd0866358ec3
lauri virtanen <lavir87@gmail.com>
2017-01-20 19:51:56 +0200

    1 done slightly better

--- OLIOT/OLIOT/Vehicles.cs
+++ OLIOT/OLIOT/Vehicles.cs
@@ -14,29 +14,41 @@ namespace OLIOT
             Tyre Hakkapeliitta = new Tyre("Hakkapeliitta", "Nokia", "205R16");
             List<Tyre> Hakkapeliitat = new List<Tyre>();
 
-            int x = 0;
+            Tyre MIC = new Tyre("Pilot","MIC",  "160R17");
+            Tyre MIC2 = new Tyre("Pilot", "MIC", "140R16");
+            List<Tyre> MICS = new List<Tyre>();
+
+            MICS.Add(MIC); MICS.Add(MIC2);
+
+            
 
             Vehicle Car1 = new Vehicle("911", "Porche",4, Hakkapeliitat);
             
 
-            for (int i = 0; i<Car1.Wheel; i++)
+            
+            for (int i = 0; i < Car1.Wheel; i++)
             {
                 Hakkapeliitat.Add(Hakkapeliitta);
             }
 
             List<Vehicle> Cars = new List<Vehicle>();
+
             Cars.Add(new Vehicle("Zonda", "Pagani", 4, Hakkapeliitat));
             Cars.Add(Car1);
+            Cars.Add(new Vehicle("Ducati", "Diavel", 2, MICS));
 
-            
-            foreach(var Vehicle in Cars)
+            int x = 0;
+            foreach (var Vehicle in Cars)
             {
+                //Prints every Vehicle in the Cars list
                 x = 0;
                 Console.WriteLine("\n"+Vehicle.OutPut());
-                foreach (var Tyre in Hakkapeliitat)
+
+                
+                foreach (var Tyre in Vehicle.Tyres)//Prints every Tyre for the current vehicle
                 {
                     if (x == 0) { Console.WriteLine("Tyres: "); x++; }
-                    Console.WriteLine("\t"+Hakkapeliitta.OutPut());
+                    Console.WriteLine("\t"+Tyre.OutPut());
                 }
             }
 
